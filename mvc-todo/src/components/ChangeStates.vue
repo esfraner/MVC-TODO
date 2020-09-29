561d4b2c2589aac11aadf3c7fb9b2ca78f1bf5a9
Franer <francisco.encinas@lambdamotive.com>
2020-09-29 17:30:08 +0200

    feat(changeStates): created new component

---
+++ mvc-todo/src/components/ChangeStates.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="ChangeStates">
+    <button @click="changeAllTodosStates" class="btn btn-outline-secondary">
+      Change
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ChangeStates",
+  props: ["todos"],
+  methods: {
+    changeAllTodosStates() {
+      console.log(
+        this.todos.map((todo) => ({ ...todo, active: !todo.active }))//send to store by using dispatch
+      );
+    },
+  },
+};
+</script>
